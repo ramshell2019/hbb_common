02c0b56090aaf2ba2049b5bb4fa4ea2a42271561
Ramshell <ramshell@outlook.com>
2025-03-04 09:31:27 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.10.193.69"];
+pub const RS_PUB_KEY: &str = "veNfadQalZ5FYYnvuVkrRGs6rkOhuqw5dviqQF1fCzs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
